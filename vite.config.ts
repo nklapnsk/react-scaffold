8d17ed836b8d6f3eac6b27df8e20b1f6e971f783
Nikolay Apanasik <nklapnsk@gmail.com>
2023-04-23 18:11:24 +0300

    feat: vitest config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
     plugins: [react()],
+    test: {
+        globals: true,
+        environment: "happy-dom",
+    },
     resolve: {
         alias: {
             src: path.resolve(__dirname, "./src"),
