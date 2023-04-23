25ad9ce3af0d49a592c6ee6b8274247d0a5988b2
Nikolay Apanasik <nklapnsk@gmail.com>
2023-04-23 18:43:58 +0300

    chore(folder-structure): add shared folder

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./app";
-import { ROOT_NODE_ID } from "./config.ts";
+import { APP_ROOT_NODE_ID } from "src/shared/config";
 
-const rootNode = document.getElementById(ROOT_NODE_ID) as HTMLElement;
+const rootNode = document.getElementById(APP_ROOT_NODE_ID) as HTMLElement;
 
 ReactDOM.createRoot(rootNode).render(
     <React.StrictMode>
---
+++ src/shared/config.ts
@@ -0,0 +1,1 @@
+export const APP_ROOT_NODE_ID = "root";
